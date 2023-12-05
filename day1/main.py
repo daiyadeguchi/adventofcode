7299ac7ff82a64887265d99d22961ee8e86f080d
daiyadeguchi <double.d.rugby@gmail.com>
2023-12-05 21:27:36 +0900

    day1 done. not sure how

--- day1/main.py
+++ day1/main.py
@@ -1,42 +1,22 @@
-# terrible conversion because no switch
-def convertSpelledNum(line) -> str:
-	if "one" in line:
-		line = line.replace("one", "1")
-	if "two" in line:
-		line = line.replace("two", "2")
-	if "three" in line:
-		line = line.replace("three", "3")
-	if "four" in line:
-		line = line.replace("four", "4")
-	if "five" in line:
-		line = line.replace("five", "5")
-	if "six" in line:
-		line = line.replace("six", "6")
-	if "seven" in line:
-		line = line.replace("seven", "7")
-	if "eight" in line:
-		line = line.replace("eight", "8")
-	if "nine" in line:
-		line = line.replace("nine", "9")
+file = open('./rsc/input', 'r')
+content = file.read()
 
-	return line
+nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
-#out = open("./rsc/output", "w")
-with open('./rsc/input') as f:
-	inputLines = f.read().splitlines()
+def getNum(str):
+	digits = []
+	for i, c in enumerate(str):
+		if c.isdigit():
+			digits.append(int(c))
+		for j, num in enumerate(nums):
+			if str[i:].startswith(num):
+				digits.append(j)
+	return digits[0] * 10 + digits[-1]
 
-	digitList = []
-	for line in inputLines:
-		line = convertSpelledNum(line)
-		#out.write(line + "\n")
-		digit = ''.join(filter(str.isdigit, line))
-		if len(digit) == 1:
-			digit = digit + digit
-		elif len(digit) > 1:
-			digit = digit[0] + digit[len(digit)-1]
+def sum(arr):
+	acc = 0
+	for i in arr:
+		acc += getNum(i)
+	return acc
 
-		#out.write(digit + "\n")
-		digitList.append(int(digit))
-
-	print(sum(digitList))
-out.close()
+print(sum(content.split()))
